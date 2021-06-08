c2955d4775bcda4e218c8f4f19093c87c4433d53
lizhijiang <189673674@qq.com>
2021-06-09 00:14:09 +0800

    feat: 策略模式编写

---
+++ DesignModel/CompositeModel/CompositeModel/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositeModel
+{
+    class Program
+    {
+        private interface IStrategy
+        {
+            void StrategyInterface();
+        }
+        
+        private class ConcreteStrategyA : IStrategy
+        {
+            public void StrategyInterface()
+            {
+                Console.WriteLine("[StrategyModel]---Func: ConcreteStrategyA");
+            }
+        }
+
+        private class ConcreteStrategyB : IStrategy
+        {
+            public void StrategyInterface()
+            {
+                Console.WriteLine("[StrategyModel]---Func: ConcreteStrategyB");
+            }
+        }
+
+        private static class ContextFactory
+        {
+            private static Dictionary<int, IStrategy> StrategyMap = new Dictionary<int, IStrategy>();
+
+            public static void BuildContextFactory(int key, IStrategy strategy)
+            {
+                if (StrategyMap.ContainsKey(key))
+                {
+                    Console.WriteLine("[Error.Strategy]---Map中已经包含key： {0}", key);
+                }
+                else
+                {
+                    StrategyMap.Add(key, strategy);
+                }
+            }
+            
+            public static IStrategy GetContextFactory(int key)
+            {
+                StrategyMap.TryGetValue(key, out var Strategy);
+                return Strategy;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            ContextFactory.BuildContextFactory(0, new ConcreteStrategyA());
+            ContextFactory.BuildContextFactory(1, new ConcreteStrategyB());
+
+            ContextFactory.GetContextFactory(0).StrategyInterface();
+            ContextFactory.GetContextFactory(1).StrategyInterface();
+            ContextFactory.GetContextFactory(2).StrategyInterface();
+        }
+    }
+}
---
+++ DesignModel/StrategyModel/StrategyModel/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrategyModel
+{
+    class Program
+    {
+        private interface IStrategy
+        {
+            void StrategyInterface();
+        }
+        
+        private class ConcreteStrategyA :IStrategy
+        {
+            public void StrategyInterface()
+            {
+                Console.WriteLine("[StrategyModel]--Func: ConcreteStrategyA");
+            }
+        }
+        
+        private class ConcreteStrategyB :IStrategy
+        {
+            public void StrategyInterface()
+            {
+                Console.WriteLine("[StrategyModel]--Func: ConcreteStrategyB");
+            }
+        }
+        
+        private static class ContextFactory
+        {
+            private static Dictionary<int, IStrategy> _strategies = new Dictionary<int, IStrategy>(2);
+            
+            public static void BuildStrategies(int key, IStrategy strategy)
+            {
+                if (_strategies.ContainsKey(key))
+                {
+                    Console.WriteLine("[Error.StrategyModel]--(添加失败，已存在对应key) key: {0}", key);
+                }
+                else
+                {
+                    _strategies.Add(key, strategy);
+                }
+            }
+
+            public static IStrategy GetStrategyByKey(int key)
+            {
+                _strategies.TryGetValue(key, out var strategy);
+                return strategy;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            ContextFactory.BuildStrategies(0, new ConcreteStrategyA());
+            ContextFactory.BuildStrategies(1, new ConcreteStrategyB());
+            
+            ContextFactory.GetStrategyByKey(0)?.StrategyInterface();
+            ContextFactory.GetStrategyByKey(1)?.StrategyInterface();
+            ContextFactory.GetStrategyByKey(2)?.StrategyInterface();
+            
+        }
+    }
+}
